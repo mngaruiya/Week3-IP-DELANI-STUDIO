4572d3c1d13ba691e441f564acdcec585a35ef0c
Martha <you@example.com>
2022-11-14 15:58:27 +0300

    mouse houver effect on portfolios

--- JS/delani.js
+++ JS/delani.js
@@ -25,4 +25,64 @@ $(document).ready(function() {
         $(this).hide();
         $('#product').toggle();
     });
+
+     //portfolio overlays
+     $(".one").mouseover(function() {
+        $("#one").show();
+    });
+    $(".one").mouseout(function() {
+        $("#one").hide();
+    });
+
+    $(".two").mouseover(function() {
+        $("#two").show();
+    });
+    $(".two").mouseout(function() {
+        $("#two").hide();
+    });
+    $(".three").mouseover(function() {
+        $("#three").show();
+    });
+    $(".three").mouseout(function() {
+        $("#three").hide();
+    });
+    $(".four").mouseover(function() {
+        $("#four").show();
+    });
+    $(".four").mouseout(function() {
+        $("#four").hide();
+    });
+    $(".five").mouseover(function() {
+        $("#five").show();
+    });
+    $(".five").mouseout(function() {
+        $("#five").hide();
+    });
+    $(".six").mouseover(function() {
+        $("#six").show();
+    });
+    $(".six").mouseout(function() {
+        $("#six").hide();
+    });
+    $(".seven").mouseover(function() {
+        $("#seven").show();
+    });
+    $(".seven").mouseout(function() {
+        $("#seven").hide();
+    });
+    $(".eight").mouseover(function() {
+        $("#eight").show();
+    });
+    $(".eight").mouseout(function() {
+        $("#eight").hide();
+    });
+
+
+
+
+
+
+
+
+
 });
